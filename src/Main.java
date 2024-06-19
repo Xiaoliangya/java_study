e0f977f601e929109fa04fdec740f36646d92e96
Xiaoliangya <574230082@qq.com>
2024-06-19 19:07:18 +0800

    更新

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter the size of the array:");
+    recursion.test(5);
     }
 }
 
+class recursion{
+
+    public static void test(int n){
+        if(n>2){
+            test(n-1);
+        }
+
+        System.out.println(n);
+    }
+ }
