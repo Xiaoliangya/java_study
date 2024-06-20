e5e01a8c89106f904f635ad9298af8aa54c8fc68
Xiaoliangya <574230082@qq.com>
2024-06-20 11:26:25 +0800

    第二天

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    recursion.test(5);
+        System.out.println(exercise.test(10));
+
     }
 }
 
-class recursion{
-
-    public static void test(int n){
-        if(n>2){
-            test(n-1);
+class exercise {
+    public static int test(int n) {
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
         }
-
-        System.out.println(n);
+        int result = arr[n - 1];
+        return result;
     }
- }
+
+    public int monker_peach(int day){
+        if (day == 10) {
+            return 1;
+        }
+        else if (day >= 1 && day <= 9) {
+            return (monker_peach(day)+1)*2;
+        }
+        else {
+            return -1;
+        }
+    }
+
+}
+
+
