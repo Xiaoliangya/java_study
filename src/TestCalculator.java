4472b7a5323585e79e2b658e9094a16a8ee02d16
Xiaoliangya <574230082@qq.com>
2024-06-22 22:56:56 +0800

    TestCaculator

--- src/TestCalculator.java
+++ src/TestCalculator.java
@@ -3,7 +3,7 @@ public class TestCalculator {
         MyCalculator calculator = new MyCalculator();
         System.out.println(calculator.calculate(2, 3)); // expected output: 5
         System.out.println(calculator.calculate(2, 3.0)); // expected output: 5.0
-        System.out.println(calculator.calculate(2.0, 3)); // expected output: 5.0
+        System.out.println(calculator.calculate(2.0, 3));
         System.out.println(calculator.calculate(2, 3, 4));
     }
 }
