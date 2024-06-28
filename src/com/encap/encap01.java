28b8653057215b915bff7ee2d3504ab61f015f48
Xiaoliangya <574230082@qq.com>
2024-06-28 23:08:59 +0800

    encapsulation

---
+++ src/com/encap/encap01.java
@@ -0,0 +1,58 @@
+package com.encap;
+
+public class encap01 {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("John");
+        person.setAge(25);
+        person.setSalary(50000.0);
+
+        String info = person.info();
+
+        System.out.println(info);
+    }
+}
+
+class Person {
+    public String name;
+    private int age;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.length() >= 2 && name.length() <= 6) {
+        this.name = name;
+    } else {
+            System.out.println("姓名长度不符合规范");
+            this.name = "Unknown";
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age >= 0 && age <= 120) {
+            this.age = age;
+        } else {
+            System.out.println("年龄不符合规范");
+            this.age = 18;
+        }
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String info() {
+        return "Name: " + name + ", Age: " + age + ", Salary: " + salary;
+    }
+}
